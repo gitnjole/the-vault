b71dfe8c4d2cb41754c71acc2a26b138912e59a2
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-10 19:48:21 +0100

    refactor(images): relocate images and update header

--- views/elements/head.php
+++ views/elements/head.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>The Vault</title>
-    <link rel="stylesheet" href="views/elements/css/fixes.css">
+    <link rel="stylesheet" href="css/fixes.css">
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
-<body class="h-full" style="background-image: url('views/images/bg_fond.gif');">
+<body class="h-full" style="background-image: url('/images/bg_fond.gif');">
 
 <div class="min-h-full">
