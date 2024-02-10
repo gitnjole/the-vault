cf1b0c30cf4f8154660a74c5dd57a57fddb0c96b
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-11 00:18:36 +0100

    style: update styling

--- views/elements/403.php
+++ views/elements/403.php
@@ -1,16 +1,19 @@
 <?php
 $heading = 'NOT AUTHORIZED';
-require basePath('views/elements/head.php');
-require basePath('views/elements/nav.php');
-require basePath('views/elements/banner.php');
+require base_path('views/elements/head.php');
+require base_path('views/elements/nav.php');
 ?>
+  <header class="bg-blue-400 shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl text-center font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
+      </div>
+  </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p> You are not authorized to view this blog, are you the correct owner of the blog?</p>
-      <p> <a href="/blogs">Go back</a></p>
+      <p> You are not authorized to view this page, are you the correct owner of the page?</p>
+      <p> <a href="/">Go back</a></p>
     </div>
   </main>
 
-<?php require basePath('views/elements/footer.php'); ?>
-
+<?php require base_path('views/elements/footer.php'); ?>
 
--- views/elements/404.php
+++ views/elements/404.php
@@ -1,9 +1,13 @@
 <?php
-/* $heading = 'NOT FOUND';
-require basePath('views/elements/head.php');
-require basePath('views/elements/nav.php');
-require basePath('views/elements/banner.php'); */
+$heading = 'NOT FOUND';
+require base_path('views/elements/head.php');
+require base_path('views/elements/nav.php');
 ?>
+  <header class="bg-red-400 shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl text-center font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
+      </div>
+  </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <p> Page not found, you searched for: <?= $_SERVER['REQUEST_URI']; ?></p>
@@ -11,4 +15,4 @@ require basePath('views/elements/banner.php'); */
     </div>
   </main>
 
-<?php /* require basePath('views/elements/footer.php');  */?>
+<?php require base_path('views/elements/footer.php'); ?>
