be2b097f37bb496559080fe01b2bd6d491ec519b
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-10 19:01:13 +0100

    feat: introduce new core functions

--- Core/functions.php
+++ Core/functions.php
@@ -1,11 +1,16 @@
 <?php
 
-function dd($value) {
-    var_dump($value);
+function abort($code = 404)
+{
+
+    http_response_code($code);
+
+    require "views/elements/{$code}.php";
+
+    die();
 }
 
-function abort($code = 404) {
-    http_response_code($code);
-    require "views/elements/{$code}.php";
-    die();
+function base_path(string $path): string
+{
+    return BASE_PATH . $path;
 }
