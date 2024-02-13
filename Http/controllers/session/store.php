1a9bbb2543f9235b9b6f8476bda9efa336eb2092
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-13 08:56:52 +0100

    refactor: rewrite to use new approach for validation and error catching

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,30 +1,18 @@
 <?php
 
-use Core\Session;
 use Core\Authenticator;
 use Http\Forms\LoginForm;
 
-$credentials = [
+$form = LoginForm::validate($credentials = [
     'username'=> $_POST['username'],
     'password'=> $_POST['password']
-];
-
-$form = new LoginForm();
-if ($form->validate($credentials)) {
-
-    if ((new Authenticator)->attempt($credentials)) {
-        redirect('/');
-    } 
-       
-    $form->addError(
-        'username',
-        'No matching username or password found.'
-    );
-}
-
-Session::flash('errors', $form->getErrors());
-Session::flash('old', [
-    'username' => $_POST['username'],
 ]);
 
-return redirect('/login');
+$signIn = (new Authenticator)->attempt($credentials);
+
+if (! $signIn) {
+    $form->addError('username', 'No matching username or password found.')
+        ->throwException();
+}
+    
+redirect('/');
