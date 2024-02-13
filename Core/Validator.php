7a3245324ec48f467e1e564591e94a8464622c09
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-13 03:29:49 +0100

    refactor(authorization): refactor primitive authorization function

--- Core/Validator.php
+++ Core/Validator.php
@@ -6,13 +6,13 @@ class Validator
 {    
     public static function isEmpty(mixed $value): bool
     {
-        return (!isset($value) || empty(trim($value))) ? true : false;
-
+        return (!isset($value) || empty(trim($value)));
     }
-    
-    public static function isLength(mixed $value,int $min = 1, int $max = 250): bool
+
+    public static function isLength(mixed $value, int $min = 1, int $max = 255): bool
     {
         $length = strlen(trim($value));
-        return ($length <= $min || $length >= $max);
+        return ($length <= $min && $length >= $max);
     }
+    
 }
