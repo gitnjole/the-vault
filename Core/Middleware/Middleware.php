aafbd1276c67f4f6d906d84dde6949baa53c37f7
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-13 02:38:56 +0100

    refactor(Middleware): update middleware resolution method and usage

--- Core/Middleware/Middleware.php
+++ Core/Middleware/Middleware.php
@@ -4,8 +4,23 @@ namespace Core\Middleware;
 
 class Middleware
 {
-    const MAP = [
+    private const MIDDLEWARE_MAP = [
         'guest' => Guest::class,
         'auth' => Auth::class
     ];
+
+    public static function resolve($key)
+    {
+        if (! $key) {
+            return;
+        }
+
+        $middleware = static::MIDDLEWARE_MAP[$key] ?? false;
+
+        if (!$middleware) {
+            throw new \Exception("No matching middleware found for {$key}");
+        }
+
+        (new $middleware)->handle();
+    }
 }
--- Core/Router.php
+++ Core/Router.php
@@ -56,11 +56,7 @@ class Router
     {
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
-                if ($route['middleware']) {
-                    $middleware = Middleware::MAP[$route['middleware']];
-
-                    (new $middleware)->handle();
-                }
+                Middleware::resolve($route['middleware']);
 
                 return require base_path($route['controller']);
             }
