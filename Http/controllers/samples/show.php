fb7ffa07fba5904d6f3f10f9cba6358910ef6feb
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-15 19:15:35 +0100

    refactor(show): update show controller for showing specific sample page

--- Http/controllers/samples/show.php
+++ Http/controllers/samples/show.php
@@ -6,13 +6,20 @@ use Core\Application;
 $db = Application::resolve(Database::class);
 
 $sample = $db->query(
-    "SELECT * FROM samples WHERE sample_id = :sample_id",
+    "SELECT 
+    s.*,
+    sl.sample_link,
+    sl.song_link
+    FROM 
+    samples s
+    LEFT JOIN 
+    sample_links sl ON s.sample_id = sl.sample_id
+    WHERE s.sample_id = :sample_id",
     [
         'sample_id' => $_GET['id']
     ]
 )->findOrFail();
 
 view('samples/show', [
-    'heading' => $sample['song_name'],
     'sample' => $sample
 ]);
